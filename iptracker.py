f98fd5f1faa75187fbd2c2eecc45edc90932e5ce
dimasgt <49423419+candraw@users.noreply.github.com>
2022-10-06 10:40:35 +0700

    Update iptracker.py
    Adding def main

--- iptracker.py
+++ iptracker.py
@@ -1,2 +1,18 @@
 import requests
 import sys
+
+def main():
+    url = "http://ip-api.com/json/"
+    if len(sys.argv) > 1:
+        # getting address from command line.
+        address = ''.join(sys.argv[1:])
+        url += address
+
+    response = requests.request("GET", url)
+    response = response.json()
+
+    if response['status'] == 'fail':
+        sys.exit(f'''
+status  :   {response['status']}
+message :   {response['message']}
+        ''')
